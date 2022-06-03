8b164060bfbb5ee513c2cdca4c9f68b70757f0d8
Bram Joosten <7580825+bramjoosten@users.noreply.github.com>
2022-06-03 10:42:44 +0200

    add formatting

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -9,68 +9,68 @@ class Item {
 class Shop {
   constructor(items = []) {
     this.items = items;
-    this.items.forEach(item => {
+    this.items.forEach((item) => {
       if (item.name === "Sulfuras" && item.quality !== 80) {
-        throw Error("Sulfuras quality has to be 80")
+        throw Error("Sulfuras quality has to be 80");
       }
     });
   }
 
   updateQuality() {
-    this.items.forEach(item => {
-      if (item.name === 'Sulfuras') return;
-      if (item.name === 'Aged Brie') {
-        this.updateItem(item, 1)
-        return
+    this.items.forEach((item) => {
+      if (item.name === "Sulfuras") return;
+      if (item.name === "Aged Brie") {
+        this.updateItem(item, 1);
+        return;
       }
-      if (item.name === 'Backstage passes') {
+      if (item.name === "Backstage passes") {
         if (item.sellIn <= 10 && item.sellIn > 5) {
           this.updateItem(item, 2);
-          return
+          return;
         }
         if (item.sellIn <= 5 && item.sellIn > 1) {
           this.updateItem(item, 3);
-          return
+          return;
         }
         if (item.sellIn <= 1) {
-          item.quality = 0
-          item.sellIn--
+          item.quality = 0;
+          item.sellIn--;
           return;
         }
       }
-      if (item.name === 'Conjured') {
+      if (item.name === "Conjured") {
         this.updateItem(item, -2);
-        return
+        return;
       }
       //default
       if (item.sellIn <= 0) {
-        this.updateItem(item, -2)
+        this.updateItem(item, -2);
       } else {
-        this.updateItem(item, -1)
+        this.updateItem(item, -1);
       }
-    })
+    });
 
     return this.items;
   }
 
   updateItem(item, qualityChange) {
-    const sum = item.quality + qualityChange
+    const sum = item.quality + qualityChange;
 
     if (sum > 50) {
-      item.quality = 50
+      item.quality = 50;
     }
     if (sum <= 50) {
-      item.quality = sum
+      item.quality = sum;
     }
     if (sum < 0) {
-      item.quality = 0
+      item.quality = 0;
     }
 
-    item.sellIn--
+    item.sellIn--;
   }
 }
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,113 +1,108 @@
 const { Shop, Item } = require("../src/gilded_rose");
 const fixtures = require("./texttest_fixture.js");
 
-describe("Gilded Rose", () => {
+describe("Normally", () => {
+  it("Once the sell by date has passed, Quality degrades twice as fast", () => {
+    const shop = new Shop([new Item("almostexpired", 0, 10)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(8);
+    expect(shop.items[0].sellIn).toBe(-1);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(6);
+    expect(shop.items[0].sellIn).toBe(-2);
+  });
 
-  describe("Normal shop behavior", () => {
+  it("The Quality of an item is never negative", () => {
+    const shop = new Shop([new Item("notsonegative", 10, 0)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(0);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(0);
+  });
 
-    it("Once the sell by date has passed, Quality degrades twice as fast", () => {
-      const shop = new Shop([new Item("almostexpired", 0, 10)])
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(8)
-      expect(shop.items[0].sellIn).toBe(-1)
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(6)
-      expect(shop.items[0].sellIn).toBe(-2)
-    })
+  it("At the end of each day our system lowers both values for every item", () => {
+    const shop = new Shop([new Item("generic item", 10, 10)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(9);
+    expect(shop.items[0].sellIn).toBe(9);
+  });
+  describe("The Quality of an item is never more than 50 and never negative (except Sulfuras)", () => {
+    let table = [];
+    fixtures.specificItemNames.forEach((item) => {
+      if (item === "Sulfuras") return;
+      table.push([item, 0, 50]);
+      table.push([item, 0, 0]);
+    });
 
-    it("The Quality of an item is never negative", () => {
-      const shop = new Shop([new Item("notsonegative", 10, 0)])
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(0)
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(0)
-    })
+    table.push(["Some generic item", 0, 50]);
+    table.push(["Some generic item", 0, 0]);
 
-    it("At the end of each day our system lowers both values for every item", () => {
-      const shop = new Shop([new Item("generic item", 10, 10)])
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(9)
-      expect(shop.items[0].sellIn).toBe(9)
-    })
-    describe("The Quality of an item is never more than 50 and never negative (except Sulfuras)", () => {
-      let table = []
-      fixtures.specificItemNames.forEach(item => {
-        if (item === 'Sulfuras') return
-        table.push([item, 0, 50])
-        table.push([item, 0, 0])
-      })
+    test.each(table)(
+      "item %s, sellIn %i, quality %i",
+      (name, sellIn, quality) => {
+        const shop = new Shop([new Item(name, sellIn, quality)]);
+        shop.updateQuality();
+        expect(shop.items[0].quality > 50).toBe(false);
+        expect(shop.items[0].quality < 0).toBe(false);
+      }
+    );
+  });
+});
 
-      table.push(["Some generic item", 0, 50])
-      table.push(["Some generic item", 0, 0])
+describe("Aged Brie", () => {
+  it("actually increases in Quality the older it gets", () => {
+    const shop = new Shop([new Item("Aged Brie", 10, 0)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(1);
+  });
+});
 
-      test.each(table)("item %s, sellIn %i, quality %i",
-        (name, sellIn, quality) => {
-          const shop = new Shop([new Item(name, sellIn, quality)])
-          shop.updateQuality()
-          expect(shop.items[0].quality > 50).toBe(false)
-          expect(shop.items[0].quality < 0).toBe(false)
-        })
+describe("Sulfuras", () => {
+  it("Is a legendary item and as such its Quality is 80 and it never alters", () => {
+    const notSoLegendaryItem = [new Item("Sulfuras", 999, 79)];
+    expect(() => new Shop(notSoLegendaryItem)).toThrow(Error);
+  });
 
-    })
-  })
+  it("being a legendary item, never has to be sold or decreases in Quality", () => {
+    const shop = new Shop([new Item("Sulfuras", 999, 80)]);
+    shop.updateQuality();
+    expect(shop.items[0].sellIn).toBe(999);
+    expect(shop.items[0].quality).toBe(80);
+  });
+});
 
-  describe("Aged Brie", () => {
+describe('"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches:', () => {
+  let shop = new Shop([new Item("Backstage passes", 10, 2)]);
+  it("Quality increases by 2 when there are 10 days", () => {
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(4);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(6);
+  });
+  it("and by 3 when there are 5 days or less", () => {
+    shop = new Shop([new Item("Backstage passes", 5, 2)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(5);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(8);
+  });
+  it("but Quality drops to 0 after the concert", () => {
+    shop = new Shop([new Item("Backstage passes", 1, 10)]);
+    expect(shop.items[0].quality).toBe(10);
+    expect(shop.items[0].sellIn).toBe(1);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(0);
+    expect(shop.items[0].sellIn).toBe(0);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(0);
+    expect(shop.items[0].sellIn).toBe(-1);
+  });
+});
 
-    it("actually increases in Quality the older it gets", () => {
-      const shop = new Shop([new Item("Aged Brie", 10, 0)])
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(1)
-    })
-  })
-
-
-
-  describe("Sulfuras", () => {
-
-    it("Is a legendary item and as such its Quality is 80 and it never alters", () => {
-      const notSoLegendaryItem = [new Item('Sulfuras', 999, 79)]
-      expect(() => new Shop(notSoLegendaryItem)).toThrow(Error);
-    })
-
-    it("being a legendary item, never has to be sold or decreases in Quality", () => {
-      const shop = new Shop([new Item('Sulfuras', 999, 80)])
-      shop.updateQuality()
-      expect(shop.items[0].sellIn).toBe(999)
-      expect(shop.items[0].quality).toBe(80)
-    })
-  })
-
-  describe("\"Backstage passes\", like aged brie, increases in Quality as its SellIn value approaches:", () => {
-    let shop = new Shop([new Item("Backstage passes", 10, 2)])
-    it("Quality increases by 2 when there are 10 days", () => {
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(4)
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(6)
-    })
-    it("and by 3 when there are 5 days or less", () => {
-      shop = new Shop([new Item("Backstage passes", 5, 2)])
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(5)
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(8)
-    })
-    it("but Quality drops to 0 after the concert", () => {
-      shop = new Shop([new Item("Backstage passes", 1, 10)])
-      expect(shop.items[0].quality).toBe(10)
-      expect(shop.items[0].sellIn).toBe(1)
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(0)
-      expect(shop.items[0].sellIn).toBe(0)
-      shop.updateQuality()
-      expect(shop.items[0].quality).toBe(0)
-      expect(shop.items[0].sellIn).toBe(-1)
-    })
-  })
-
-  it("\"Conjured\", items degrade in Quality twice as fast as normal items", () => {
-    const shop = new Shop([new Item('Conjured', 10, 10)])
-    shop.updateQuality()
-    expect(shop.items[0].quality).toBe(8)
-  })
+describe("Conjured", () => {
+  it("items degrade in Quality twice as fast as normal items", () => {
+    const shop = new Shop([new Item("Conjured", 10, 10)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(8);
+  });
 });
